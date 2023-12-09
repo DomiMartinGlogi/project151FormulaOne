a5d5677f3abcb56547b6c9956ee9b942e4cf6b68
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-12-09 10:12:58 +0100

    Added Initial Data and Annotations

---
+++ src/main/resources/db/migration/V1_2_INSERT_TEAMS.sql
@@ -0,0 +1,13 @@
+INSERT INTO TEAM
+(NAME, COUNTRY) VALUES
+                    ('Red Bull Racing', 'Austria'),
+                    ('Mercedes', 'Germany'),
+                    ('Ferrari', 'Italy'),
+                    ('McLaren', 'United Kingdom'),
+                    ('Aston Martin', 'United Kingdom'),
+                    ('Alpine', 'France'),
+                    ('Williams', 'United Kingdom'),
+                    ('Alpha Tauri', 'Italy'),
+                    ('Alfa Romeo', 'Switzerland'),
+                    ('Haas F1', 'United States'),
+                    ('Independent', 'N/A');
