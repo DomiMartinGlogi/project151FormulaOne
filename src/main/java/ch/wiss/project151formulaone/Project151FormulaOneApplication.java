eb75cac0151d886ee819e7cf914bed423d4620a6
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-12-09 09:55:13 +0100

    Added Initial Data and Annotations

--- src/main/java/ch/wiss/project151formulaone/Project151FormulaOneApplication.java
+++ src/main/java/ch/wiss/project151formulaone/Project151FormulaOneApplication.java
@@ -2,12 +2,16 @@ package ch.wiss.project151formulaone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableCaching
+@EnableAsync
 public class Project151FormulaOneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Project151FormulaOneApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Project151FormulaOneApplication.class, args);
+    }
 
 }
