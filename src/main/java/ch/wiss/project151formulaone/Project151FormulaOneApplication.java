283ba6d5c713906fdcddf8740e6780ea71f1905f
DomiMartinGlogi <93612122+DomiMartinGlogi@users.noreply.github.com>
2023-12-09 08:48:13 +0100

    Initial commit

---
+++ src/main/java/ch/wiss/project151formulaone/Project151FormulaOneApplication.java
@@ -0,0 +1,13 @@
+package ch.wiss.project151formulaone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project151FormulaOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project151FormulaOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/wiss/project151formulaone/Project151FormulaOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wiss.project151formulaone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project151FormulaOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
